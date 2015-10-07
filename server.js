36dcdb9702668560e92c61158f2c239f662671e9
Drew Miller <drew666@gmail.com>
2015-10-07 15:23:01 -0700

    Keywatcher proof of concept

---
+++ server.js
@@ -0,0 +1,24 @@
+
+var http = require('http');
+var node_static = require('node-static');
+var socket = require('./lib/socket');
+var path = require('path');
+
+var src = '/src';
+
+// 2. Static files server
+var static_directory = new node_static.Server(path.join(__dirname, src));
+
+// 3. SockJS http server
+var server = http.createServer();
+server.addListener('request', function(req, res) {
+  static_directory.serve(req, res);
+});
+server.addListener('upgrade', function(req,res){
+  res.end();
+});
+
+socket(server);
+
+console.log(' [*] Listening on 0.0.0.0:9999' );
+server.listen(9999, '0.0.0.0');
